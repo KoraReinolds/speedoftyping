aefbd5bc8d75822cc24cf2d77cf171fc1dd63652
KoraReinolds <s.shurin94@gmail.com>
2023-12-20 16:57:56 +0300

    Display text
    - Added 'TextField' component that breaks the text into words
    and letters and displays them

---
+++ src/components/TextField.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{ text: string }>()
+
+const words = computed(() => props.text.split(' '))
+
+</script>
+
+<template>
+  <div class="text">
+    <span v-for="word in words" class="word">
+      <span v-for="letter in word">
+        {{ letter }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.text {
+  max-width: 300px;
+  font-size: 1.3em;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.word {
+  margin-right: 0.3em;
+  color: #888;
+}
+</style>
