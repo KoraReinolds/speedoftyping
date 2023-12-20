ee3316aa2f53218b2951779addeacad59e0ab16a
KoraReinolds <s.shurin94@gmail.com>
2023-12-20 18:25:38 +0300

    Add a cursor to visualize the current position

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -28,15 +28,21 @@ const displayWords = computed(() => {
   })
 })
 
+const cursor = ref({
+  wordIndex: 0,
+  letterIndex: 1,
+})
+
 </script>
 
 <template>
   <div class="text">
-    <span v-for="word in displayWords" class="word">
-      <span v-for="letter in word" :class="{
+    <span v-for="(word, wordIndex) in displayWords" class="word">
+      <span v-for="(letter, letterIndex) in word" :class="{
         letter: true,
         error: letter[1] && letter[1] !== letter[0],
-        correct: letter[1] && letter[1] === letter[0]
+        correct: letter[1] && letter[1] === letter[0],
+        cursor: cursor.wordIndex === wordIndex && cursor.letterIndex === letterIndex
       }">
         {{ letter[0] || letter[1] }}
       </span>
@@ -46,15 +52,19 @@ const displayWords = computed(() => {
 
 <style scoped>
 .text {
-  max-width: 300px;
-  font-size: 1.3em;
+  max-width: 800px;
+  font-size: 1.8em;
   display: flex;
   flex-wrap: wrap;
 }
 
 .word {
-  margin-right: 0.3em;
+  margin-right: 0.5em;
   color: #888;
+}
+
+.letter {
+  margin-right: 0.05em;
 }
 
 .letter.correct {
@@ -63,5 +73,28 @@ const displayWords = computed(() => {
 
 .letter.error {
   color: rgb(236, 117, 117);
+  position: relative;
+}
+
+.letter.cursor::before {
+  content: '';
+  position: absolute;
+  width: 2px;
+  height: 1em;
+  background-color: #dbe128;
+  transform: translate(-1px, 0.2em);
+  animation: fadeInOut 1.5s infinite alternate;
+}
+
+@keyframes fadeInOut {
+
+  0%,
+  100% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 1;
+  }
 }
 </style>
